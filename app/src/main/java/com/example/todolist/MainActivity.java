91850066fa8d7b87d70217adcfb26a53c54eead7
Armaan <armaankohli@berkeley.edu>
2020-06-02 18:43:32 -0700

    work on layout

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText itemET;
+    private Button bt;
+    private ListView itemsList;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        itemET = findViewById(R.id.item_edit_text)
     }
 }
